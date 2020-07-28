f1be41471403992e60163e3d6cb68014366fc217
giridhar-ippili <giridhar.ippili@hotmail.com>
2020-07-28 11:06:02 -0400

    Initial Commit

---
+++ src/main/kotlin/com/example/BuddyKeeper/BuddyKeeperApplication.kt
@@ -0,0 +1,11 @@
+package com.example.BuddyKeeper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuddyKeeperApplication
+
+fun main(args: Array<String>) {
+	runApplication<BuddyKeeperApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/BuddyKeeper/BuddyKeeperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.BuddyKeeper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuddyKeeperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
